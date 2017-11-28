23e502836c94333e92884e5ca488027f81a77b34
sank <uplife1980@163.com>
2017-11-29 01:42:11 +0800

    前端最后稳定版11.28

--- pages/maishu/maishu.js
+++ pages/maishu/maishu.js
@@ -24,6 +24,7 @@ Page({
         that.setData({
         isbn:res.result
         })
+
       },
       fail: function(res) {},
       complete: function(res) {
@@ -39,7 +40,8 @@ Page({
     input2:true,
     isbn: '',
     img:'',
-    display:"none"
+    display:"none",
+   
   },
   /**
    * 生命周期函数--监听页面加载
@@ -56,59 +58,11 @@ Page({
       input2: that
     })
   },
+ 
   onLoad: function () {
     
 },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
  
   
 })
