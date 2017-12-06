b28c71eebdc93aefdf0b93146f93a5dfad751e9a
sank <uplife1980@163.com>
2017-12-06 21:50:59 +0800

    合并团队12.6

--- pages/maishu/maishu.js
+++ pages/maishu/maishu.js
@@ -1,7 +1,7 @@
 // pages/maishu/maishu.js
 var app = getApp();
 Page({
- 
+
   data: {
     input1: false,
     input2: true,
@@ -11,7 +11,7 @@ Page({
     display1: "none",
     display2: "none",
     phone: "",
-    error:''
+    error: ''
   },
   iwantpic: function () {
     var that = this
@@ -78,37 +78,35 @@ Page({
     this.setData({
       input2: that
     })
-      },
-  formCheck:function(e){
-    var that=this
-    if (that.data.img[0] != null && that.data.img2[0] != null && e.detail.value.book != 0 && e.detail.value.isbn != 0 && e.detail.value.phone != 0&&(e.detail.value.borrow||e.detail.value.buy)!=0)
-    {
-      that.setData({
-        error: ""
-      })
-      formSubmit(e)
-     
-      }
+  },
+  formCheck: function (e) {
+    var that = this
+    if (that.data.img[0] != null && that.data.img2[0] != null && e.detail.value.book != 0 && e.detail.value.isbn != 0 && e.detail.value.phone != 0 && (e.detail.value.borrow || e.detail.value.buy) != 0) {
+      
+        // formSubmit(e)
+
+    }
     else
-    that.setData({
-      error:"信息没有填写完整,请检查!"
-    })
+      that.setData({
+        error: "信息没有填写完整,请检查!"
+      })
   },
   formSubmit: function (e) {
+    formCheck();
     var that = this;
-    var date=new Date() //9+4
+    var date = new Date() //9+4
     var request_id = date.getTime() + Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
     wx.uploadFile({
       url: 'http://localhost:8082/BookShare/upload/uploading',
       filePath: that.data.img[0],
       name: 'imgsrc',
-      header: { 
+      header: {
         'content-type': 'multipart/form-data'
-         },
-      formData: {"onlycode": "001"},
+      },
+      formData: { "onlycode": "001" },
       success: function (res) {
         console.log(res)
-       },
+      },
       fail: function (res) { },
       complete: function (res) { },
     })
@@ -116,10 +114,11 @@ Page({
       url: 'http://localhost:8082/BookShare/upload/uploading',
       filePath: that.data.img2[0],
       name: 'imgsrc2',
-      header: { 
+      header: {
         'content-type': 'multipart/form-data'
-         },
-      formData: {"onlycode" : "001",
+      },
+      formData: {
+        "onlycode": "001",
         name: e.detail.value.book,//书的名字
         information: e.detail.value.isbn,//isbn号 
         borrowable: e.detail.value.maishu,//卖书的开关，1是卖，0是不卖
@@ -127,22 +126,25 @@ Page({
         rentable: e.detail.value.chuzu,//出租的开关，同卖书
         sale_price: e.detail.value.buy//买书的价格,
       },
-      success: function (res) { 
-        console.log("123")
+      success: function (res) {
+
+
       },
       fail: function (res) { },
-      complete: function (res) { },
+      complete: function (res) {
+        console.log(e.detail.value.chuzu);
+      },
     })
-    
+
     wx.request({
       url: 'http://localhost:8082/BookShare/rentable/bookapplication',
       data: {
-        name        :   e.detail.value.book,//书的名字
-        information :   e.detail.value.isbn,//isbn号 
-        borrowable  :   e.detail.value.maishu,//卖书的开关，1是卖，0是不卖
-        rent_price  :   e.detail.value.borrow,//出租的价格
-        rentable    :   e.detail.value.chuzu,//出租的开关，同卖书
-        sale_price  :   e.detail.value.buy//买书的价格
+        name: e.detail.value.book,//书的名字
+        information: e.detail.value.isbn,//isbn号 
+        borrowable: e.detail.value.maishu,//卖书的开关，1是卖，0是不卖
+        rent_price: e.detail.value.borrow,//出租的价格
+        rentable: e.detail.value.chuzu,//出租的开关，同卖书
+        sale_price: e.detail.value.buy//买书的价格
       },
       header: {
         //'content-type'  :   'application/json'
@@ -153,7 +155,7 @@ Page({
 
         console.log(res.data.result)
       },
-      fail: function (res) {},
+      fail: function (res) { },
       complete: function (res) {
       },
     })
@@ -172,15 +174,15 @@ Page({
       },
       method: "GET",
 
-      success: function(res) {
-        if(res.data.fail==0){
+      success: function (res) {
+        if (res.data.fail == 0) {
           that.setData({
-            phone:res.data.phone
+            phone: res.data.phone
           })
         }
       },
-      fail: function(res) {},
-      complete: function(res) {},
+      fail: function (res) { },
+      complete: function (res) { },
     })
   },
 
