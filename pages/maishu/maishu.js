d431ef34804b57669f410b08fc80a9eb909ae29e
Echohuiyin <lmr09232007@163.com>
2017-12-24 17:47:48 +0800

    删除了缓存，删除了立体图的上传

--- pages/maishu/maishu.js
+++ pages/maishu/maishu.js
@@ -7,7 +7,6 @@ Page({
     input2: true,
     isbn: '',
     img: '',
-    img2: '',
     display1: "none",
     display2: "none",
     phone: "",
@@ -29,42 +28,14 @@ Page({
       complete: function (res) { },
     })
   },
-  iwantpic2: function () {
-    var that = this
-    wx.chooseImage({
-      count: 0,
-      sizeType: ["compressed"],
-      sourceType: [],
-      success: function (res) {
-        that.setData({
-          display2: "block",
-          img2: res.tempFilePaths
-        })
-      },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
-  },
   iwantcode: function (e) {
     var that = this;
     wx.scanCode({
       onlyFromCamera: false,
       success: function (res) {
         that.setData({
-          isbn : res.result)
+          isbn : res.result
         })
-        // wx.request({
-        //   url: "http://api.jisuapi.com/isbn/query?appkey=85c75335fa427fe4&isbn="+that.data.isbn,
-        //   data: {},
-        //   header: {},
-        //   method:"POST",
-        //   success: function(res) {
-        //     console.log(res)
-        //     //setData 设置书的信息，让一个request一起传过去
-        //     },
-        //   fail: function(res) {},
-        //   complete: function(res) {},
-        // })
       },
       fail: function (res) { },
       complete: function (res) {
@@ -110,7 +81,6 @@ Page({
     var that = this;
     var date = new Date() //9+4
     var request_id = date.getTime()*10000 + Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
-  console.log(request_id)
     wx.uploadFile({
       url: 'http://localhost:8082/BookShare/upload/image',
       filePath: that.data.img[0],
@@ -121,23 +91,6 @@ Page({
          },
       formData: {
         "onlycode"  : request_id,
-        "imagename" : "first"
-        },
-      success: function (res) { },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
-    wx.uploadFile({
-      url: 'http://localhost:8082/BookShare/upload/image',
-      filePath: that.data.img2[0],
-
-      name: 'imagefile',
-      header: { 
-        'content-type': 'multipart/form-data'
-         },
-      formData: {
-        "onlycode"  :   request_id,
-        "imagename" :   "second"
         },
       success: function (res) { },
       fail: function (res) { },
@@ -147,13 +100,13 @@ Page({
     wx.request({
       url: 'http://localhost:8082/BookShare/rentable/bookapplication',
       data: {
-        name        :   e.detail.value.book,//书的名字
+        userid      :   request_id,//以后会用openID替代
         isbn        :   e.detail.value.isbn,//isbn号 
         rentbtn     :   e.detail.value.rentbtn,//出租的开关，同卖书
         sellbtn     :   e.detail.value.sellbtn,//卖书的开关，1是卖，0是不卖
         rent_price  :   (e.detail.value.rentbtn == false) ? 0 : e.detail.value.borrow,//出租的价格
         sale_price  :   (e.detail.value.sellbtn == false) ? 0 : e.detail.value.buy,//买书的价格
-        onlycode    :   request_id,
+        onlycode    :   request_id,//上传的图片名关键部分
         origin_tel  :   e.detail.value.phone
       },
       header: {
@@ -182,7 +135,7 @@ Page({
                       pic     : res.data.result.pic,
                       author  : res.data.result.author,   
                       summary : res.data.result.summary,
-                      isbn    : res.data.result.isbn      //还有其它字段          
+                      isbn    : res.data.result.isbn      //还有其它字段需要补充      
                   },
                   header: {
                     //'content-type'  :   'application/json'
@@ -206,19 +159,17 @@ Page({
   },
   onLoad: function () {
     var that = this
-
+   //这个模块移动到补全信息页面
     wx.request({
-      //url: 'https://localhost/request' + 'method?=getUserPhone',
-      url: '',
+      url: 'http://localhost:8082/BookShare/user/getUserPhone',
       data: {
-        "user": app.globalData.userInfo.nickName
+        //"user": app.globalData.userInfo.nickName
+        "userid" : "15135999504716428"
       },
-
       header: {
         'Content-Type': 'application/json'
       },
       method: "GET",
-
       success: function (res) {
         if (res.data.fail == 0) {
           that.setData({
