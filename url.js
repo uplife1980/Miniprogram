90621bff5c989f56f12c83b7ae21673b863eb6b0
Echohuiyin <lmr09232007@163.com>
2018-01-15 15:06:06 +0800

    修改了url，解决了不能查看远程图片的bug

--- url.js
+++ url.js
@@ -1,11 +1,9 @@
 function Url(){
-  var url ="http://115.159.24.14/"
+  var url ="http://115.159.24.14/bookshare/"
   //var url = "http://localhost:8082/BookShare/"
   //var url = "http://192.168.3.140:8082/BookShare/"
   return  url;
 }
-
-
 module.exports = {
   Url:Url
 }
