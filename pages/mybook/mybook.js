3603d4000b7cd035f78129190b40238c37d68d70
sank <uplife1980@163.com>
2018-09-23 19:17:50 +0800

    修复补全图书全部跑到第一本的bug

--- pages/mybook/mybook.js
+++ pages/mybook/mybook.js
@@ -15,7 +15,7 @@ Page({
     bookid: 0,
     day_color: "black",
     hidelist: [],
-    showModalStatus: false 
+    showModalStatus: [false] 
   },
   /**
    * 生命周期函数--监听页面加载
@@ -67,6 +67,7 @@ Page({
         }
         for (var i = data.bought.length + data.renting.length + data.outdate.length, j = 0; j < data.rentable.length; i++ , j++) {
           data.rentable[j].title = data.booktitle[i]
+            data.rentable[j].showCompleteSheet=0
         }
         for (var i = data.bought.length + data.renting.length + data.outdate.length + data.rentable.length, j = 0; j < data.saling.length; i++ , j++) {
           data.saling[j].title = data.booktitle[i]
@@ -260,7 +261,6 @@ Page({
   },
   fillContent:function(e){
     var that=this
-
     var val=e.detail.value
    if(!val.title||!val.author||!val.publisher||!val.price)
      that.showWarn("请不要漏下任何一项哦")
@@ -268,7 +268,7 @@ Page({
     wx.request({
       url: Url.Url() + 'rentable/alternobook',
       data:{
-        isbn:e.target.id,
+        isbn:e.currentTarget.id,
         title:e.detail.value.title,
         publisher:e.detail.value.publisher,
         author:e.detail.value.author,
@@ -311,58 +311,67 @@ Page({
     })
   },
 
-  // 模态弹窗
-  powerDrawer: function (e) {
-    var currentStatu = e.currentTarget.dataset.statu;
-    this.util(currentStatu)
-  },
-  util: function (currentStatu) {
-    /* 动画部分 */
-    // 第1步：创建动画实例 
-    var animation = wx.createAnimation({
-      duration: 200, //动画时长 
-      timingFunction: "linear", //线性 
-      delay: 0 //0则不延迟 
-    });
+powerDrawer:function(e)
+{
+  var that=this
+  var neverborrow=that.data.neverborrow_list
+  neverborrow[e.currentTarget.dataset.index].showCompleteSheet=1
+  that.setData({
+    neverborrow_list:neverborrow
+  })
+}
+  // // 模态弹窗
+  // powerDrawer: function (e) {
+  //   var currentStatu = e.currentTarget.dataset.statu;
+  //   this.util(currentStatu)
+  // },
+  // util: function (currentStatu) {
+  //   /* 动画部分 */
+  //   // 第1步：创建动画实例 
+  //   var animation = wx.createAnimation({
+  //     duration: 200, //动画时长 
+  //     timingFunction: "linear", //线性 
+  //     delay: 0 //0则不延迟 
+  //   });
 
-    // 第2步：这个动画实例赋给当前的动画实例 
-    this.animation = animation;
+  //   // 第2步：这个动画实例赋给当前的动画实例 
+  //   this.animation = animation;
 
-    // 第3步：执行第一组动画 
-    animation.opacity(0).rotateX(-100).step();
+  //   // 第3步：执行第一组动画 
+  //   animation.opacity(0).rotateX(-100).step();
 
-    // 第4步：导出动画对象赋给数据对象储存 
-    this.setData({
-      animationData: animation.export()
-    })
+  //   // 第4步：导出动画对象赋给数据对象储存 
+  //   this.setData({
+  //     animationData: animation.export()
+  //   })
 
-    // 第5步：设置定时器到指定时候后，执行第二组动画 
-    setTimeout(function () {
-      // 执行第二组动画 
-      animation.opacity(1).rotateX(0).step();
-      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象 
-      this.setData({
-        animationData: animation
-      })
+  //   // 第5步：设置定时器到指定时候后，执行第二组动画 
+  //   setTimeout(function () {
+  //     // 执行第二组动画 
+  //     animation.opacity(1).rotateX(0).step();
+  //     // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象 
+  //     this.setData({
+  //       animationData: animation
+  //     })
 
-      //关闭 
-      if (currentStatu == "close") {
-        this.setData(
-          {
-            showModalStatus: false
-          }
-        );
-      }
-    }.bind(this), 200)
+  //     //关闭 
+  //     if (currentStatu == "close") {
+  //       this.setData(
+  //         {
+  //           showModalStatus: false
+  //         }
+  //       );
+  //     }
+  //   }.bind(this), 200)
 
-    // 显示 
-    if (currentStatu == "open") {
-      this.setData(
-        {
-          showModalStatus: true
-        }
-      );
-    }
-  }
+  //   // 显示 
+  //   if (currentStatu == "open") {
+  //     this.setData(
+  //       {
+  //         showModalStatus: true
+  //       }
+  //     );
+  //   }
+  // }
 
 })
